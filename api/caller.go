793586fd484bb8465b2e648e0117b3b3f492250b
Gabriel Soares Campanário <gabriel.camp96@hotmail.com>
2022-03-05 18:30:06 -0300

    implementing API callers

--- api/caller.go
+++ api/caller.go
@@ -1,12 +1,71 @@
 package api
 
 import (
-	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 
+	"github.com/gabaghul/geolocation-golang/consts"
 	"github.com/gabaghul/geolocation-golang/models"
 )
 
 // GetCityInfoByName returns info from requested city
-func GetCityInfoByName(ctx context.Context, cityName string) (city models.GeoAPICity, err error) {
+func GetCityInfoByName(cityName string) (cities models.GeoAPICities, err error) {
+	client := consts.GetHTTPClient()
+	headers := http.Header{}
+
+	req, err := http.NewRequest("GET", consts.CitiesEndpoint, nil)
+	if err != nil {
+		return
+	}
+
+	queryStrings := req.URL.Query()
+
+	queryStrings.Add("namePrefix", cityName)
+	headers.Add("X-RapidAPI-Key", consts.GetApiKey())
+
+	req.URL.RawQuery = queryStrings.Encode()
+	req.Header = headers
+
+	res, err := client.Do(req)
+	if err != nil || res.StatusCode != http.StatusOK {
+		return
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &cities)
+
+	return
+}
+
+// GetNearbyCities returns all nearby cities based on a city reference ID
+func GetNearbyCities(cityID int64) (cities models.GeoAPICities, err error) {
+	client := consts.GetHTTPClient()
+	headers := http.Header{}
+
+	req, err := http.NewRequest("GET", consts.GetNearbyCitiesEndpoint(cityID), nil)
+	if err != nil {
+		return
+	}
+
+	headers.Add("X-RapidAPI-Key", consts.GetApiKey())
+	req.Header = headers
+
+	res, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(body, &cities)
+
 	return
 }
--- consts/consts.go
+++ consts/consts.go
@@ -1,13 +1,21 @@
 package consts
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 	"sync"
+)
+
+const (
+	rootEndpoint   = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+	CitiesEndpoint = rootEndpoint + "/cities"
 )
 
 var (
 	once   sync.Once
 	apiKey string
+	client *http.Client
 )
 
 func GetApiKey() string {
@@ -16,4 +24,16 @@ func GetApiKey() string {
 	})
 
 	return apiKey
+}
+
+func GetHTTPClient() *http.Client {
+	once.Do(func() {
+		client = &http.Client{}
+	})
+
+	return client
+}
+
+func GetNearbyCitiesEndpoint(id int64) string {
+	return fmt.Sprintf(CitiesEndpoint+"/%d/nearbyCities", id)
 }
